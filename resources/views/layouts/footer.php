a1c4ca7074fbafe2de447e2d3d39f047e4866912
Jose Farroñan <jfarronansaca99@gmail.com>
2023-12-25 23:15:54 -0500

    Cambios actualzaidos en el front(diseño)

--- resources/views/layouts/footer.php
+++ resources/views/layouts/footer.php
@@ -4,18 +4,9 @@
             <img src="{{ asset('images/logo-footer.svg') }}" alt="">
         </div>
         <div class="text-center redeSociales d-flex justify-content-center">
-            <a href="https://www.facebook.com/rhnube" target="_blank"
-                class="footer-social-link-three w-inline-block"><img
-                    src="https://assets-global.website-files.com/64bfdae61395c9772f504e2f/64c1561b6ec2c62e15f4bafb_facebook.svg"
-                    loading="lazy" alt="FB white" class="boton-fb"></a>
-            <a href="https://www.instagram.com/rhnubeoficial/" target="_blank"
-                class="footer-social-link-three w-inline-block"><img
-                    src="https://assets-global.website-files.com/64bfdae61395c9772f504e2f/64c1561b0ccd6781d9011d5e_instagram.svg"
-                    loading="lazy" alt="IG white"></a>
-            <a href="https://www.youtube.com/@RHNUBE" target="_blank"
-                class="footer-social-link-three w-inline-block"><img
-                    src="https://assets-global.website-files.com/64bfdae61395c9772f504e2f/64c1561ba1eb3b24c7f702c5_youtube.svg"
-                    loading="lazy" alt="YT white"></a>
+            <a href="https://www.facebook.com/rhnube" target="_blank" class="footer-social-link-three w-inline-block"><img src="https://assets-global.website-files.com/64bfdae61395c9772f504e2f/64c1561b6ec2c62e15f4bafb_facebook.svg" loading="lazy" alt="FB white" class="boton-fb"></a>
+            <a href="https://www.instagram.com/rhnubeoficial/" target="_blank" class="footer-social-link-three w-inline-block"><img src="https://assets-global.website-files.com/64bfdae61395c9772f504e2f/64c1561b0ccd6781d9011d5e_instagram.svg" loading="lazy" alt="IG white"></a>
+            <a href="https://www.youtube.com/@RHNUBE" target="_blank" class="footer-social-link-three w-inline-block"><img src="https://assets-global.website-files.com/64bfdae61395c9772f504e2f/64c1561ba1eb3b24c7f702c5_youtube.svg" loading="lazy" alt="YT white"></a>
         </div>
 
         <div class="text-center enlacesExt my-4 d-flex justify-content-center">
--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -35,12 +35,14 @@
 
                         <div class="row text-center mx-3">
                             {{-- <img src="{{ asset('images/group2.png') }}" alt="Paso 2" /> --}}
-                            <div class="col-md-12 my-2 px-3">
+                            <div class="col-md-12 my-3 px-3">
                                 <div class="row align-items-center">
                                     <div class="selectCountry col-sm-6 ">
-                                        <h5 class="my-3 mx-3 pr-3 text-sm-right text-center">Elige tu país: </h5>
+                                        <h5 class="my-3 mx-3 pr-3 text-sm-left text-center">Elige tu país </h5>
                                     </div>
-                                    <div class="col-sm-6 mx-0 px-0">
+                                    <div class="selectCountry col-sm-1 ">
+                                    </div>
+                                    <div class="col-sm-5 mx-0 px-0">
                                         <select name="idCountry" required id="idCountry"
                                             class="form-control text-center">
                                             <option value="">Seleccione su país</option>
@@ -51,12 +53,14 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-12  my-2 cantEmp">
+                            <div class="col-md-12  my-3 cantEmp">
                                 <div class="row align-items-center">
                                     <div class="selectCountry col-sm-6 ">
-                                        <h5 class="my-3 mx-3 pr-3 text-sm-right text-center">Cant. empleados:
+                                        <h5 class="my-3 mx-3 pr-3 text-sm-left text-center">N° de empleados
                                     </div>
-                                    <div class="col-sm-6  px-0">
+                                    <div class="selectCountry col-sm-1 ">
+                                    </div>
+                                    <div class="col-sm-5  px-0">
                                         <div class="rowAdd m-auto d-flex rounded-pill justify-content-center">
                                             <div class="dismin">-</div>
                                             <input type="number" name="quanty" min="1" required value="1"
@@ -67,12 +71,14 @@
                                 </div>
                             </div>
 
-                            <div class="periodo col-md-12 my-2 ">
+                            <div class="periodo col-md-12 my-3">
                                 <div class="row align-items-center">
                                     <div class="selectCountry col-sm-6 ">
-                                        <h5 class="my-3 mx-3 pr-3 text-sm-right text-center">Periodo de pago: </h5>
+                                        <h5 class="my-3 mx-3 pr-3 text-sm-left text-center">Periodo de pago </h5>
                                     </div>
-                                    <div class="col-sm-6 mx-0 px-0">
+                                    <div class="selectCountry col-sm-1 ">
+                                    </div>
+                                    <div class="col-sm-5 mx-0 px-0">
                                         <select name="idPla" id="idPla" class="form-control text-center"
                                             id="">
                                             @foreach ($plans as $plan)
@@ -83,7 +89,7 @@
                                 </div>
                             </div>
 
-                            <div class="resPago table-responsive w-100 text-center mb-0">
+                            <div class="resPago table-responsive w-100 text-center my-3 mb-0">
                                 {{-- <img src="{{ asset('images/group4.png') }}" alt="Paso 4" /> --}}
 
                                 <table class="table w-100 mt-3 pb-0 mb-0">
@@ -102,26 +108,26 @@
                                             <td class="priceUn">$ {{ $categoryPlan->price }}</td>
                                             <td class="subtotal">$ {{ $categoryPlan->price }}</td>
                                         </tr>
-                                        <tr>
+                                        {{-- <tr>
                                             <td></td>
 
                                             <td></td>
-                                            <td style="text-align: right;"><b>SUBTOTAL</b></td>
+                                            <td style="text-align: right;">Subtotal</td>
                                             <td class="subtotal">$ {{ $categoryPlan->price }}</td>
-                                        </tr>
+                                        </tr> --}}
                                         <tr>
                                             <td></td>
 
                                             <td></td>
-                                            <td style="text-align: right;"><b>IMPUESTO</b></td>
+                                            <td style="text-align: right;">Impuesto</td>
                                             <td class="tax">$ 0.00</td>
                                         </tr>
 
                                         <tr>
                                             <td class="py-4"></td>
                                             <td class="py-4"></td>
-                                            <td style="text-align: right;" class="py-4"><b>TOTAL</b></td>
-                                            <td class="total py-4">$ {{ $categoryPlan->price }}</td>
+                                            <td style="text-align: right;" class="py-4 titleTotal"><b>TOTAL</b></td>
+                                            <td class="total py-4 titleTotal"><b>$ {{ $categoryPlan->price }}</b></td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -134,7 +140,7 @@
                     </div>
 
                 </div>
-                <div class="buttonBuy text-center my-4 ">
+                <div class="buttonBuy text-center mt-0 mb-5 ">
                     <button class="btn btn-primary showPayment w-100 rounded-pill" id="payWithStripe">Pagar</button>
                 </div>
         </form>
@@ -146,6 +152,11 @@
 
 
 <style>
+    header,
+    footer {
+        display: none;
+    }
+
     input::-webkit-outer-spin-button,
     input::-webkit-inner-spin-button {
         display: none !important;
@@ -163,19 +174,24 @@
     }
 
     .formPage .container {
-        width: 50%;
+        width: 60%;
     }
 
     .showPayment {
         background: #08d7d4;
-        border: 1px solid #08d7d4;
+        border-radius: 10px !important;
         border: none;
+        font-size: 25px;
+        text-transform: uppercase;
+        font-weight: bold;
+        height: 50px;
     }
 
     .card {
         border: 1px solid #eff0f6;
         box-shadow: 0 2px 7px 0 rgba(20, 20, 43, 0.06);
         border-radius: 24px;
+        padding-top: 35px !important;
     }
 
     .selectMeth h5,
@@ -183,25 +199,26 @@
     .periodo h5,
     .selectCountry h5,
     .resPago h5 {
-        color: #464b50;
-        font-size: 18px;
+        color: #b0b4b9;
+        font-size: 25px;
+        font-weight: 400;
+        border-bottom: 2px solid #dee2e6;
     }
 
     .rowAdd,
     select {
         background-color: #fff;
         width: 100%;
-        height: 41px;
+        height: 52px !important;
         align-items: center;
         border-radius: 50rem !important;
+        border: 1px solid #08d7d4 !important;
     }
 
-    .rowAdd {
-        border: 1px solid #ced4da;
-    }
+
 
     select {
-        font-size: 18px !important;
+        font-size: 25px !important;
     }
 
     .addmin,
@@ -228,12 +245,13 @@
         width: 100px;
         text-align: center;
         border: none;
+        font-size: 22px;
     }
 
     .periodo button,
     .selectMeth button {
         border: 2px solid #EFF0F6;
-        font-size: 18px;
+        font-size: 25px;
     }
 
     .periodo button.btnSelec,
@@ -242,6 +260,24 @@
         color: #fff !important;
         font-weight: 600;
         border-color: #5654e6;
+    }
+
+    table {
+        font-size: 25px;
+    }
+
+    .titleTotal {
+        color: #08d7d4;
+    }
+
+    .table td,
+    .table th {
+        padding-top: 1.45rem;
+        padding-bottom: 1.45rem;
+    }
+
+    .table thead th {
+        border-top: none;
     }
 
     @media screen and (max-width:1000px) {
@@ -327,7 +363,7 @@
         function updatePrices(response) {
 
             $("td.tax").text("$ " + response.tax);
-            $("td.subtotal").text("$ " + response.subtotal);
+            //$("td.subtotal").text("$ " + response.subtotal);
             $("td.priceUn").text("$ " + response.price);
             $("td.quanty").text($("#valor").val());
             $("td.total").text("$ " + response.total);
@@ -338,52 +374,6 @@
             $valor.val(Math.max(1, parseInt($valor.val()) + increment));
             changePer();
         });
-
-
-        // function seleccionarPeriodo(boton) {
-        //     // Convert boton to a jQuery object
-        //     var boton = $(boton);
-
-        //     // Obtener todos los botones dentro del contenedor
-        //     var botones = $('.btn');
-
-        //     // Remover la clase btnSelec de todos los botones
-        //     botones.removeClass('btnSelec');
-
-        //     // Agregar la clase btnSelec solo al botón clicado
-        //     boton.addClass('btnSelec');
-
-        //     var planId = boton.data('plan-id');
-        //     $("input#idPla").val(planId);
-        //     var quantity = $("#valor").val();
-        //     var csrfToken = "{{ csrf_token() }}";
-
-        //     // Hacer la solicitud AJAX al servidor
-        //     $.ajax({
-        //         url: "{{ route('update_prices') }}",
-        //         method: "POST",
-        //         data: {
-        //             _token: csrfToken,
-        //             quantity: quantity,
-        //             plan_id: planId
-        //         },
-        //         success: function(response) {
-        //             // Actualizar la información en la página
-        //             $("td.tax").text("$ " + response.tax);
-        //             $("td.subtotal").text("$ " + response.subtotal);
-        //             $("td.priceUn").text("$ " + response.price);
-        //             $("td.quanty").text(quantity);
-        //             $("td.total").text("$ " + response.total);
-        //         },
-        //         error: function(xhr, status, error) {
-        //             console.log(xhr);
-        //         },
-        //         complete: function() {
-        //             console.log("Actualización de precios completada");
-        //         }
-        //     });
-        // }
-
 
     });
 </script>
